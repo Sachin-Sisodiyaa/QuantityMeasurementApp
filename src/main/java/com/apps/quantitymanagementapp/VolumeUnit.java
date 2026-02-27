c4b45c65c76ebd089bb822381ac634f6c5f39e50
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-02-27 15:13:36 +0530

    [Sachin] : Added VolumeUnit.java file

--- src/main/java/com/apps/quantitymanagementapp/VolumeUnit.java
+++ src/main/java/com/apps/quantitymanagementapp/VolumeUnit.java
@@ -1,7 +1,6 @@
 package com.apps.quantitymanagementapp;
 
 public enum VolumeUnit implements IMeasurable {
-	
 	LITRE(1.0), MILLILITRE(0.001), GALLON(3.78541);
 
 	private final double conversionFactor;
