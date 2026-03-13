5fba06f18bbf05c4f47371ddaea78c1d078f98e8
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-03-13 15:33:40 +0530

    [Sachin] : Added QuantityMeasurementException.java file

---
+++ src/main/java/com/apps/quantitymeasurementapp/exception/QuantityMeasurementException.java
@@ -0,0 +1,15 @@
+
+package com.apps.quantitymeasurementapp.exception;
+
+public class QuantityMeasurementException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public QuantityMeasurementException(String message) {
+		super(message);
+	}
+
+	public QuantityMeasurementException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
