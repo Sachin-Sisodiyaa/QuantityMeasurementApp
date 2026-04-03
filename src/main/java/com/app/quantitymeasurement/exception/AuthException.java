e8837a9846e1d7ce160e338a4bf808afeadfd62e
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-04-04 00:32:04 +0530

    [Sachin] : Added AuthException.java file

--- src/main/java/com/app/quantitymeasurement/exception/AuthException.java
+++ src/main/java/com/app/quantitymeasurement/exception/AuthException.java
@@ -1,7 +1,20 @@
 package com.app.quantitymeasurement.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class AuthException extends RuntimeException {
+    private final HttpStatus status;
+
     public AuthException(String message) {
+        this(HttpStatus.UNAUTHORIZED, message);
+    }
+
+    public AuthException(HttpStatus status, String message) {
         super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 }
